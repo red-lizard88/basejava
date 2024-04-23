f6cb404f28a1dbe87a314621a361e646b25d9978
”Ilya <i.liashchanka@gmail.com>
2024-04-23 19:31:44 +0300

    Made new methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,84 +5,55 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        Resume rTemp = new Resume();
-        rTemp.uuid = "Нет такого элемента в массиве.";
-        for (Resume resume : storage) {
-            if (resume != null) {
-                if (resume.uuid.equals(uuid)) {
-                    rTemp = resume;
-                    break;
-                }
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
             }
         }
-        return rTemp;
+        return null;
 
     }
 
     void delete(String uuid) {
-        for (Resume resume : storage) {
-            if (resume != null) {
-                if (resume.uuid.equals(uuid)) {
-                    resume.uuid = null;
-                    break;
-                }
+        int k = -1;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                k = i;
+                break;
             }
         }
-
-        Resume[] storageTemp = new Resume[10000];
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] != null) {
-                if (storage[i].uuid != null) {
-                    for (int j = 0; j < storageTemp.length - 1; j++) {
-                        if (storageTemp[j] == null) {
-                            storageTemp[j] = storage[i];
-                            break;
-                        }
-                    }
-                }
+        if (k >= 0) {
+            for (int j = k; j < size - 1; j++) {
+                storage[j] = storage[j + 1];
             }
-        }
-        storage = storageTemp;
+            storage[size - 1] = null;
+            size--;
+        } else System.out.println("Неверный uuid.");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int count = 0;
-        for (Resume resume : storage) {
-            if (resume != null) {
-                count++;
-            }
-        }
-        Resume[] storageTemp = new Resume[count];
-        System.arraycopy(storage, 0, storageTemp, 0, storageTemp.length);
-
-        return storageTemp;
+        Resume[] resumes = new Resume[size];
+        System.arraycopy(storage, 0, resumes, 0, size);
+        return resumes;
     }
 
     int size() {
-        int count = 0;
-        for (Resume resume : storage) {
-            if (resume != null) {
-                count++;
-            }
-        }
-        return count;
+        return size;
     }
 }
