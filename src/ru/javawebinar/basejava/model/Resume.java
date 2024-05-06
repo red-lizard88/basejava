a763610c39931ab681e878a80898fa7238b07d9f
”Ilya <i.liashchanka@gmail.com>
2024-05-06 22:20:35 +0300

    Exeptions

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -1,13 +1,38 @@
 package ru.javawebinar.basejava.model;
 
+import java.util.Objects;
+import java.util.UUID;
+
 /**
  * Initial resume class
  */
-public class Resume {
+public class Resume implements Comparable<Resume>{
+
+
 
     // Unique identifier
-    private String uuid;
+    private final String uuid;
+    public Resume() {
+        this(UUID.randomUUID().toString());
+    }
 
+
+    public Resume(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return Objects.equals(uuid, resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
     @Override
     public String toString() {
         return uuid;
@@ -17,7 +42,8 @@ public class Resume {
         return uuid;
     }
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
+    @Override
+    public int compareTo(Resume o) {
+        return uuid.compareTo(o.uuid);
     }
 }
