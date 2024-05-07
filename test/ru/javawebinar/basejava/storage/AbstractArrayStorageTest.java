583987b3b3433436de9ae9e7d4c7c5d2b1bdf65a
”Ilya <i.liashchanka@gmail.com>
2024-05-07 20:17:12 +0300

    Made first test

---
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,60 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+public class AbstractArrayStorageTest {
+    private Storage storage = new ArrayStorage();
+
+    public static final String UUID_1 = "uuid1";
+    public static final String UUID_2 = "uuid2";
+    public static final String UUID_3 = "uuid3";
+    @Before
+    public void SetUp() throws Exception{
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
+
+    @Test
+    public void size() {
+        Assert.assertEquals(3, storage.size());
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() {
+        storage.get("dummy");
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}
