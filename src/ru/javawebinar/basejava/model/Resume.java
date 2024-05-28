5f5a49cf22ff3fb45d1d90f9813b77223e69e084
”Ilya <i.liashchanka@gmail.com>
2024-05-28 20:40:35 +0300

    Made task 9

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -10,24 +10,16 @@ import java.util.UUID;
  */
 public class Resume implements Comparable<Resume> {
 
-
     // Unique identifier
     private final String uuid;
 
     private final String fullName;
+
     private final Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
     private final Map<SectionType, Section> sections = new EnumMap<>(SectionType.class);
 
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
-    }
-
-    public String getContact(ContactType type){
-        return contacts.get(type);
-    }
-
-    public Section getSection(SectionType type){
-        return sections.get(type);
     }
 
     public Resume(String uuid, String fullName) {
@@ -37,26 +29,48 @@ public class Resume implements Comparable<Resume> {
         this.fullName = fullName;
     }
 
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getContact(ContactType type) {
+        return contacts.get(type);
+    }
+
+    public Section getSection(SectionType type) {
+        return sections.get(type);
+    }
+
+    public void addContact(ContactType type, String value) {
+        contacts.put(type, value);
+    }
+
+    public void addSection(SectionType type, Section section) {
+        sections.put(type, section);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         Resume resume = (Resume) o;
-        return Objects.equals(uuid, resume.uuid) && Objects.equals(fullName, resume.fullName);
+
+        if (!uuid.equals(resume.uuid)) return false;
+        return fullName.equals(resume.fullName);
+
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(uuid, fullName);
+        int result = uuid.hashCode();
+        result = 31 * result + fullName.hashCode();
+        return result;
     }
 
     @Override
     public String toString() {
-        return uuid + "(" + fullName + ")";
-    }
-
-    public String getUuid() {
-        return uuid;
+        return uuid + '(' + fullName + ')';
     }
 
     @Override
